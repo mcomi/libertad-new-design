5b508c2894dd9a18e81d5e5a6060c86e78af7bff
mcomix <mcomi@hotmail.com>
2020-11-11 11:05:26 -0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  purge: ["./**/*.html", "./assets/src/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        "regal-blue": "#243c5a",
+        "reddish-orange": "#f26522",
+        "light-navy-blue-two": "#305395",
+        "macaroni-and-cheese": "#f1ca30",
+        "white-two": "#f0f0f0",
+        "black-two": "#1e1e1e",
+        "dark-grey-blue": "#2c315f",
+        "light-navy-blue": "#325699",
+        "peacock-blue": "#005398",
+        "macaroni-and-cheese-two": "#f2ca30",
+        marigold: "#fbb902",
+        "dark-grey-blue-two": "#2b315f",
+        "dark-slate-blue": "#223c6b",
+        "pinkish-grey": "#d1d1d1",
+        "light-navy-blue-three": "#2f5394",
+        "pinkish-grey-two": "#c5c5c5",
+        "white-three": "#ebe9e9",
+        "twilight-blue": "#0d3750",
+        "greyish-brown": "#545454",
+        "greyish-brown-two": "#3f3f3f",
+        "black-three": "#313131",
+        "light-navy-blue-four": "#2f5495",
+        "piss-yellow": "#d2b41a",
+      },
+      fontFamily: {
+        body: ["Work Sans"],
+      },
+      height: {
+        "bg-slide": "70vh",
+        "sm-slide": "40vh",
+      },
+    },
+  },
+};
